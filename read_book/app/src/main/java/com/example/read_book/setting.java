76d57be9e885f85a615cd09f47e967320582d08e
nqnphuong <ngnhatphuong.a.h@gmail.com>
2022-06-10 08:35:17 +0700

    python

--- read_book/app/src/main/java/com/example/read_book/setting.java
+++ read_book/app/src/main/java/com/example/read_book/setting.java
@@ -2,7 +2,8 @@ package com.example.read_book;
 
 public class setting {
 
-    public String URL = "http://192.168.1.7:5000/"; //myhomw
+//    public String URL = "http://192.168.1.7:5000/"; //myhomw
 //    public String URL = "http://192.168.43.63:5000/"; //my 4G
 //    public String URL = "http://192.168.1.12:5000/";
+    public String URL = "http://192.168.0.108:5000/"; //myhomw
 }
